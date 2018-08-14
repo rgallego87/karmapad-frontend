4b38fee3f96694989bfb19a8bf27d707b53f93f4
Rafa Gallego <rgdmax@gmail.com>
2018-08-14 19:42:09 +0200

    Commenting and ordering things

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
+// Angular base
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+// Components
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+// Pages
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { SignupPageComponent } from './pages/signup-page/signup-page.component';
 import { PrivatePageComponent } from './pages/private-page/private-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-import { FormsModule } from '@angular/forms';
+// PrimeNG
 import { AccordionModule } from 'primeng/accordion';   
 import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
 import { ButtonModule } from 'primeng/button'; 
-
+// Guards
 import { RequireAnonGuard } from './guards/require-anon.guard';
 import { RequireUserGuard } from './guards/require-user.guard';
 import { InitAuthGuard } from './guards/init-auth.guard';
