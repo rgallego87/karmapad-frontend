357817d2788cb73d9be67260fd5cff9dfd1cf63e
Rafa Gallego <rgdmax@gmail.com>
2018-08-21 14:43:09 +0200

    Testing different styles import from app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css',
+  '/node_modules/primeicons/primeicons.css',
+  '/node_modules/primeng/resources/themes/omega/theme.css',
+  '/node_modules/primeng/resources/primeng.min.css']
 })
 export class AppComponent implements OnInit {
 
